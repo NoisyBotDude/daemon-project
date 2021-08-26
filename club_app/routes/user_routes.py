138d692710d22bb69441a745e00d2895d0618acb
prajwalita07 <hazarikaprajwalita@gmail.com>
2021-08-26 14:00:45 +0530

    added club info page

--- club_app/routes/user_routes.py
+++ club_app/routes/user_routes.py
@@ -15,7 +15,7 @@ def create_blueprint(cluster):
 
     @user.route("/clubs/<string:club_name>")
     def club_info(club_name):
-        club_info = DataBase.find_club(club_name)
+        club_info = DataBase.find_club_name()
         return render_template("user/club_info.html", club_info=club_info)
 
     return user
